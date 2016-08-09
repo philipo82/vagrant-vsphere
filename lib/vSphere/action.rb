f42c6d2330f35f26264cfdbc1161377693f43890
Jakub Gawryjolek <jgawry@gmail.com>
2016-08-09 17:07:52 -0400

    test2

--- lib/vSphere/action.rb
+++ lib/vSphere/action.rb
@@ -44,9 +44,9 @@ module VagrantPlugins
                 next
               end
 
-	#      b3.use ProvisionVSphere
               b3.use Provision
               b3.use SyncedFolders
+              b3.use ProvisionVSphere
             end
           end
         end
@@ -192,7 +192,7 @@ module VagrantPlugins
       autoload :MessageNotRunning, action_root.join('message_not_running')
       autoload :PowerOff, action_root.join('power_off')
       autoload :PowerOn, action_root.join('power_on')
-      # autoload :ProvisionVSphere, action_root.join('provision_vsphere')
+      autoload :ProvisionVSphere, action_root.join('provision_vsphere')
     end
   end
 end
--- lib/vSphere/action/provision_vsphere.rb
+++ lib/vSphere/action/provision_vsphere.rb
@@ -1,4 +1,5 @@
 require 'rbvmomi'
+require 'vSphere/util/vim_helpers'
 
 module VagrantPlugins
   module VSphere
@@ -9,14 +10,39 @@ module VagrantPlugins
           @app = app
         end
 
+
         def call(env)
-          @app.call env
-        end
 
-        private
+        	vmdk_spec = RbVmomi::VIM::FileBackedVirtualDiskSpec(
+	            capacityKb: 1024 * 1024,
+        	    adapterType: 'lsiLogic',
+	            diskType: 'thin'
+	        )
 
-        def provision_vsphere()
-	    puts 'running provisioneerrrrr!'
+		device = RbVmomi::VIM::VirtualDisk(
+		    capacityInKB: 1024 * 1024,
+                )
+
+		device_config_spec = RbVmomi::VIM::VirtualDeviceConfigSpec(
+		    device: device,
+		    operation: RbVmomi::VIM::VirtualDeviceConfigSpecOperation('add')
+	        )
+	        
+	        vm_config_spec = RbVmomi::VIM::VirtualMachineConfigSpec (
+	            deviceChange: [device_config_spec]
+                )
+
+	        create_disk_task = RbVmomi::VIM.VirtualDiskManager.CreateVirtualDisk_Task ( 
+			datacenter: @connection.serviceInstance.find_datacenter
+	        	name => "[TRUST-01] jakub-test-with-parted/jakub-test-with-parted-2.vmdk"
+	        	spec: vmdk_spec
+	        ).wait_for_completion
+	        
+	        return if env[:machine].state.id == :not_created
+	        vm = get_vm_by_uuid env[:vSphere_connection], env[:machine]
+	        return if vm.nil?
+	        
+	        vm.ReconfigVM_Task(spec: vm_config_spec).wait_for_completion
         end
       end
     end
